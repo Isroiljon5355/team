52d6eb70ee2532bb38f8462285a7974c744644a3
xakimovotabek200 <xakimovoatbek@gmail.com>
2023-03-14 23:09:14 +0500

    Add to card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react'
+import Card from './Components/Card/Card'
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Card/>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import "./Card.css"
+import {data} from "../../data"
+
+function Card() {
+    return (
+      <div>
+          <div className="Sentenses">
+              <h1>Gaming Product Corner</h1>
+              <p>Compete with 100 players on a remote island for winner takes showdown known <br /> issue t feels at home on the head-up skin strategic.</p>
+          </div>
+      <section>
+          { data.map( ( item ) =>
+          {
+              return <div key={ item.id }>
+                  <div className="card">
+                       <div className="item">
+              <div className="price">
+                          <p>{ item.price}</p>
+          </div>
+              <img src={item.Image} alt="" />
+                  <div className="priceIcon">
+                      {item.icon}
+              </div>
+              <div className="Text">
+                   <h3>
+                  {item.text.h3}
+                  </h3>
+                  <div className="TextPrice">
+                              <p>{ item.text.p}</p>
+                              <del>{ item.text.del}</del>
+                  </div>
+              <button className="btn">Add to card</button>
+              </div>
+          </div> 
+                  </div>
+              </div>
+          })} 
+            </section>
+             </div>
+  )
+}
+
+export default Card
