f9882f8bda852f092a410d915fc0e779b7d2a95f
tolibjon1015 <tolibboyjamoliddinov45@gmail.com>
2023-03-20 09:51:24 +0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import Cards from "./components/cards/Cards";
-import data from "./data";
-import DataniPropsi from "./DataniPropsi";
-import Form from "./Form";
 
 function App() {
   return (
     <div className="App">
       <Cards />
-      <DataniPropsi data={data} />
-      <Form />
-      <h1 className=" text-red-900 text-center">salom</h1>
     </div>
   );
 }
