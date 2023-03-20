d6b340a2f384e874726249878b17cb1a4913e21f
Isroiljon5355 <isroiljon5355@gmail.com>
2023-03-20 10:29:43 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import React from "react";
+
 function App() {
-  return (
-    <>
-      <h1 className=" text-red-900 text-center">salom</h1>
-    </>
-  );
+  return <div>App</div>;
 }
 
 export default App;
