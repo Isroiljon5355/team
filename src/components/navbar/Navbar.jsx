8bbed241de24ddde707049eb225664508db1f915
samandar0806 <lutfullayev0806@gmail.com>
2023-03-20 09:56:26 +0500

    navbar-add

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
         defer
       ></script>
       <div class="min-h-screen ">
-        <div class="antialiased bg-gray-100 dark-mode:bg-gray-900">
+        <div class="antialiased bg-gray-200 dark-mode:bg-gray-900">
           <div class=" text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 ">
             <div
               x-data="{ open: true }"
@@ -34,7 +34,7 @@ function Navbar() {
               </div>
               <nav class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
                 <a
-                  class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 text-white hover:duration-500 hover:text-[#F16262] focus:outline-none focus:shadow-outline"
+                  class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 text-white hover:duration-500 hover:text-[#F16262] focus:outline-none"
                   href="#"
                 >
                   Blog
