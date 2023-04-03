b215ad33d66a7130780df38474e10ae733f14689
Muhammadali2oo6 <xabibullayevali2@gmail.com>
2023-04-03 15:01:25 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Medium from "./medium/Medium.jsx";
 function App() {
+  
+
   return (
     <div>
-      <Medium />
+        <Medium />
     </div>
   );
 }
--- src/medium/Medium.jsx
+++ src/medium/Medium.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 function Section() {
   return (
-    <div className="  ">
-      <div className="">
+    <div className="">
+      <div className="lg:flex justify-around">
         <div className="relative mt-[20px] ">
           <img
             className="rounded-lg md:w-[60%]"
@@ -10,7 +10,7 @@ function Section() {
             alt=""
           />
           <div className=" w-[92%] md:w-[52%] absolute top-[40%] left-[4%] backdrop-blur-[33px] p-[20px] rounded-lg text-white bg-[rgba(0,0,0,.6)] sm:grid-cols-1  ">
-            <h1 className="text-[30px] font-extrabold ">
+            <h1 className="text-[30px] font-extrabold">
               Assassin's Creed Valhalla
             </h1>
             <div className="flex ">
@@ -20,9 +20,7 @@ function Section() {
             <div className="flex justify-between text-center">
               <div className="">
                 <p className="mr-[169px] font-medium">Activison:</p>
-                <p className="font-medium ">
-                  PS4, PS5, Xbox One, Xbox Series 
-                </p>
+                <p className="font-medium ">PS4, PS5, Xbox One, Xbox Series</p>
               </div>
               <div className="w-[120px] py-[5px] border-none overflow-hidden rounded-lg  bg-gradient-to-r from-red-600 to-indigo-500 hover:bg-gradient-to-l to-indigo-500 from-red-600 text-white">
                 <button>Order Now</button>
@@ -48,9 +46,7 @@ function Section() {
               <div className="flex justify-between text-center">
                 <div className="paragraf">
                   <p className="mr-[176px] font-medium">Activison:</p>
-                  <p className="font-medium">
-                    PS4, PS5, Xbox One, Xbox Series
-                  </p>
+                  <p className="font-medium">PS4, PS5, Xbox One, Xbox Series</p>
                 </div>
                 <div className="w-[120px] py-[10px] border-none overflow-hidden rounded-lg  bg-gradient-to-r from-red-600 to-indigo-500 hover:bg-gradient-to-l to-indigo-500 from-red-600 text-white">
                   <button>Order Now</button>
@@ -65,7 +61,7 @@ function Section() {
                 src="https://hostim-html.themetags.com/assets/img/home3/ft-3.jpg"
                 alt=""
               />
-              <div className="w-[92%] md:w-[52%] absolute top-[73%] mx-auto left-[4%] backdrop-blur-[33px] p-[20px] rounded-lg text-white bg-[rgba(0,0,0,.6)]">
+              <div className="w-[92%] md:w-[52%] sm:w-[53%] absolute top-[68%] mx-auto left-[4%] backdrop-blur-[33px] p-[20px] rounded-lg text-white bg-[rgba(0,0,0,.6)]">
                 <h1 className="text-[15px] font-extrabold ">
                   Assassin's Creed Valhalla
                 </h1>
@@ -87,7 +83,7 @@ function Section() {
                 src="https://hostim-html.themetags.com/assets/img/home3/ft-4.jpg"
                 alt=""
               />
-              <div className="w-[92%] md:w-[52%]  absolute top-[73%] mx-auto left-[4%] backdrop-blur-[33px] p-[20px] rounded-lg text-white bg-[rgba(0,0,0,.6)]">
+              <div className="w-[92%] sm:w-[53%] md:w-[52%]  absolute top-[68%] mx-auto left-[4%] backdrop-blur-[33px] p-[20px] rounded-lg text-white bg-[rgba(0,0,0,.6)]">
                 <h1 className="text-[15px] font-extrabold ">
                   Assassin's Creed Valhalla
                 </h1>
