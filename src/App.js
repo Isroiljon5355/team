2b0f166c6f0cf3afdd30e0c817f0d5b27589e326
ibrohim5009 <ibrohimabdurayimov5@gmail.com>
2023-04-10 13:27:28 +0500

    eeeeeee

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Footer from "./components/footer/Footer";
+import { data } from "./data";
+import { Context } from "./components/Cards/Context";
+import Card from "./components/Cards/Card";
 
 function App() {
   return (
     <div>
+      <Context.Provider value={data}>
+        <Card />
+      </Context.Provider>
       <Footer />
     </div>
   );
